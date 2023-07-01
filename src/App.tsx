d9c1118755ca21371ab1a2484d44cac9a28722c3
Ferdinand Estoque <festoqufx@gmail.com>
2023-07-01 21:59:38 +0800

    cvxzzcv

---
+++ src/App.tsx
@@ -0,0 +1,90 @@
+import { FormEvent, useState } from "react";
+import { AccountForm } from "./AccountForm";
+import { AddressForm } from "./AddressForm";
+import { useMultistepForm } from "./useMultistepForm";
+import { UserForm } from "./UserForm";
+
+type FormData = {
+  firstName: string;
+  lastName: string;
+  age: string;
+  street: string;
+  city: string;
+  state: string;
+  zip: string;
+  email: string;
+  password: string;
+};
+
+const INITIAL_DATA: FormData = {
+  firstName: "",
+  lastName: "",
+  age: "",
+  street: "",
+  city: "",
+  state: "",
+  zip: "",
+  email: "",
+  password: "",
+};
+
+function App() {
+  const [data, setData] = useState(INITIAL_DATA);
+  function updateFields(fields: Partial<FormData>) {
+    setData((prev) => {
+      return { ...prev, ...fields };
+    });
+  }
+  const { steps, currentStepIndex, step, isFirstStep, isLastStep, back, next } =
+    useMultistepForm([
+      <UserForm {...data} updateFields={updateFields} />,
+      <AddressForm {...data} updateFields={updateFields} />,
+      <AccountForm {...data} updateFields={updateFields} />,
+    ]);
+
+  function onSubmit(e: FormEvent) {
+    e.preventDefault();
+    if (!isLastStep) return next();
+    alert("Successful Account Creation");
+  }
+
+  return (
+    <div
+      className="max-w-md  mix-blend-multiply w-max min-w-min mt-40 bg-green-700 text-sm font-sans antialiased border-solid shadow-lg  rounded-tr-2xl rounded-tl-2xl rounded-br-2xl rounded-bl-2xl p-4 m-auto"
+      //   style={{
+      //     position: "relative",
+      //     background: "white",
+      //     border: "1px solid black",
+      //     padding: "2rem",
+      //     margin: "1rem",
+      //     borderRadius: ".5rem",
+      //     fontFamily: "Arial",
+      //     maxWidth: "max-content",
+      //   }}
+    >
+      <form onSubmit={onSubmit}>
+        <div style={{ position: "absolute", top: ".5rem", right: ".5rem" }}>
+          {currentStepIndex + 1} / {steps.length}
+        </div>
+        {step}
+        <div
+          style={{
+            marginTop: "1rem",
+            display: "flex",
+            gap: ".5rem",
+            justifyContent: "flex-end",
+          }}
+        >
+          {!isFirstStep && (
+            <button type="button" onClick={back}>
+              Back
+            </button>
+          )}
+          <button type="submit">{isLastStep ? "Finish" : "Next"}</button>
+        </div>
+      </form>
+    </div>
+  );
+}
+
+export default App;
